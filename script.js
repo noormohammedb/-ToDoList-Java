92be9f14382ce267c044851a55cefd99aaf9f707
Noor Mohammed B <nooru.mohad@gmail.com>
2020-05-08 19:58:39 +0530

    js updating

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-// document.write("<h1>Hello World!</h1>")
-function btn(){
-    console.log("click");
-    console.log(document.URL);
-    document.URL("google.com");
-    console.log(document.URL);
+// // document.write("<h1>Hello World!</h1>")
+// function btn(){
+//     console.log("click");
+//     console.log(document.URL);
+//     document.URL("google.com");
+//     console.log(document.URL);
     
+// }
+
+const paper = document.querySelector(".paper");
+var list = document.createElement("li")
+
+function btn(){
+    document.querySelector(".list");
 }
