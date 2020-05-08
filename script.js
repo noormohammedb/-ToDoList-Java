abc499c29561018bb0149d8edd617b1eb1995c76
Noor Mohammed B <nooru.mohad@gmail.com>
2020-05-08 19:46:29 +0530

    basic layout

---
+++ script.js
@@ -0,0 +1,8 @@
+// document.write("<h1>Hello World!</h1>")
+function btn(){
+    console.log("click");
+    console.log(document.URL);
+    document.URL("google.com");
+    console.log(document.URL);
+    
+}
